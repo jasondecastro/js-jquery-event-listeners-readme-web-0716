bcfeaa978309b266b64652586a7a9a3929d3b2f2
Jason Decastro <jasonrdecastro@gmail.com>
2016-08-31 12:02:55 -0400

    completed jquery event listeners

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
-//define functions here
+function getIt() {
+  alert("Hey!")
+}
+
+function frameIt() {
+  $('img').addClass('tasty')
+}
+
+function pressIt() {
+  alert("hey")
+}
+
+function submitIt() {
+  alert("Your form is going to be submitted now.")
+}
 
 $(document).ready(function(){
 
-// call functions here
+$('p').on('click', function(){
+  getIt()
+});
+
+$('img').on('load', function(){
+  frameIt()
+});
+
+$('input').on('keydown', function(key){
+  if (key.which == 71) {
+    pressIt()
+  }
+});
+
+$('form').on('submit', function(){
+  submitIt()
+});
 
 });
